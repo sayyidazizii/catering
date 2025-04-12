e119dad1b38f5a017c2f7135c98890fbda3ff81b
sayyidazizii <sayyidsyafiq234@gmail.com>
2025-04-12 13:34:13 +0700

    enhance(app.blade): add navbar link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="">{{ __('Home') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">{{ __('Merchant') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">{{ __('Menu') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
